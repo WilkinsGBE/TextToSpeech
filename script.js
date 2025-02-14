bd1d41008790dedc583037c5fc8886a79568f115
Wilkins Saintil <158526628+WilkinsGBE@users.noreply.github.com>
2025-02-14 15:32:27 -0500

    fixing the voice issue on mobile

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
 
 let voicesSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () => {
+// Function to load voices properly
+function loadVoices() {
     voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
+    
+    if (voices.length === 0) {
+        setTimeout(loadVoices, 100);
+        return;
+    }
 
-    voices.forEach((voice, i) => (voicesSelect.options[i]) = new Option(voice.name, i))
-};
+    speech.voice = voices[0]; 
+    voicesSelect.innerHTML = ""; 
 
+    voices.forEach((voice, i) => {
+        let option = new Option(voice.name, i);
+        voicesSelect.add(option);
+    });
+}
+
+// Ensure voices are loaded
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = loadVoices;
+}
+
+// Also call loadVoices in case onvoiceschanged doesn't fire
+loadVoices();
+
+// Change voice when the user selects a different option
 voicesSelect.addEventListener("change", () => {
     speech.voice = voices[voicesSelect.value];
 });
 
+// Speak the text on button click
 document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech);
+    
+    if (speech.text.trim() !== "") {
+        window.speechSynthesis.cancel(); 
+        window.speechSynthesis.speak(speech);
+    }
 });
